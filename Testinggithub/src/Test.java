9aa908976c3ae5da66d79e02961e011c90038ed7
Mohan Kumar Velaga <mohan5v5988@gmail.com>
2016-03-04 17:02:17 -0800

    printout d.

--- Testinggithub/src/Test.java
+++ Testinggithub/src/Test.java
@@ -1,18 +1,18 @@
 
 public class Test {
 	public void a() {
-
+		System.out.println("this is d function");
 	}
 
 	public void b() {
-
+		System.out.println("this is d function");
 	}
 
 	public void c() {
-
+		System.out.println("this is d function");
 	}
 
 	public void d() {
-
+		System.out.println("this is d function");
 	}
 }
